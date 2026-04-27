a682b3f5c3eb306f1316f041097c6f1906c91b2a
Enrique Arias <eariasvel@gmail.com>
2026-04-27 15:58:48 -0500

    Update console log messages in sandbox script

--- sandbox-get-story-from-localstorage.js
+++ sandbox-get-story-from-localstorage.js
@@ -1,5 +1,9 @@
-console.log("Holaaaassssss!!!!! 12.29");
+console.log("Holaa desde el script de sandbox!!!!!, 3.59pm");
 
 function miFuncion(param1, param2) {
   console.log(param1, param2);
 }
+
+const getStoryFromLocalStorage = (idHighlightStoryForYou, classes, website) => {
+  console.log("NUEVO SCRIPT:", idHighlightStoryForYou, classes, website);
+};
