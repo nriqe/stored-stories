f74f2d3eafaf372c98d03d6a163d330ca7139ab1
Enrique Arias <eariasvel@gmail.com>
2026-04-27 16:15:37 -0500

    Update sandbox-get-story-from-localstorage.js

--- sandbox-get-story-from-localstorage.js
+++ sandbox-get-story-from-localstorage.js
@@ -1,9 +1,18 @@
-console.log("Holaa desde el script de sandbox!!!!!, 3.59pm");
+console.log("Holaa desde el script de sandbox!!!!!, 4.15pm");
 
-function miFuncion(param1, param2) {
-  console.log(param1, param2);
-}
-
-const getStoryFromLocalStorage = (idHighlightStoryForYou, classes, website) => {
-  console.log("NUEVO SCRIPT:", idHighlightStoryForYou, classes, website);
+const getStoryFromLocalStorage = (
+  localStorageJustSeenStoriesKey,
+  maxStoriesFromLs,
+  idHighlightStoryForYou,
+  classes,
+  website
+) => {
+  console.log(
+    "NUEVO SCRIPT:",
+    localStorageJustSeenStoriesKey,
+    maxStoriesFromLs,
+    idHighlightStoryForYou,
+    classes,
+    website
+  );
 };
