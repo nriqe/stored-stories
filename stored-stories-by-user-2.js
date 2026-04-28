179a9cd4bade9294f8f75d19128d45422c74fd9e
Enrique Arias <eariasvel@gmail.com>
2026-04-28 18:21:52 -0500

    Refactor function to get stories by user

--- stored-stories-by-user-2.js
+++ stored-stories-by-user-2.js
@@ -1,3 +1,18 @@
-const miFuncion = () => {
-  console.log("DESDE SCRIPT EXTERNO", window?.tp?.user?.isUserValid());
+const getStoriesByUser = (
+  idUserStoriesContainer,
+  idEmptyStoriesContainer,
+  maxStories,
+  website,
+  strClasses
+) => {
+  const classes = JSON.parse(strClasses);
+  console.log(
+    "DESDE SCRIPT EXTERNO",
+    window?.tp?.user?.isUserValid(),
+    idUserStoriesContainer,
+    idEmptyStoriesContainer,
+    maxStories,
+    website,
+    classes
+  );
 };
