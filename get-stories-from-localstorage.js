ebabacd0eeec744cbd266f4b250b46d72cc27b34
Enrique Arias <eariasvel@gmail.com>
2026-05-11 17:12:02 -0500

    Update get-stories-from-localstorage.js

--- get-stories-from-localstorage.js
+++ get-stories-from-localstorage.js
@@ -152,7 +152,7 @@ const getStoriesFromLocalStorage = async (
   const difference = (storiesQty - storiesFromLs.length) + 1;
   console.log('NRO DE NOTAS EN LS:', storiesFromLs.length, storiesQty, difference);
 
-  if (difference > 1) {
+  if (difference > 0) {
     const lastPublishedStories = await getLastPublishedStories(source, query, arcSite, deployment, difference);
     console.log('NOTAS DE RELLENO: ',lastPublishedStories);
   }  
