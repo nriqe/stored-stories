589eeacc3af30c879b7b0052021eec1f7ea52685
Enrique Arias <e.arias@rpalatam.com.pe>
2026-05-22 15:29:01 -0500

    feat: js mundial 2026

--- world-cup-2026-ec.js
+++ world-cup-2026-ec.js
@@ -1,4 +1,4 @@
-const getWorldCupMatches = (
+const getWorldCupMatches = async (
   idMatchesContainer,
   idCarouselBtnPrev,
   idCarouselBtnNext,
@@ -16,4 +16,36 @@ const getWorldCupMatches = (
     idCardCounter,
     jsonPath
   );
+
+  const getWorldCupMatchesFromApi = async () => {
+    const currentTime = new Date().getTime();
+
+    try {
+      const response = await fetch(`${jsonPath}}?${currentTime}`,
+        {
+          method: "GET",
+          cache: "no-store",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      /*if (
+        !response.status ||
+        response.status !== successfullResponse ||
+        !response.ok
+      ) {
+        throw new Error("Error al obtener las notas de usuario.");
+      }*/
+
+      const result = await response.json();
+      console.log('PARTIDOS DESDE EL JSON:',result);
+      return result;
+    } catch (error) {
+      throw new Error("ERROR AL TRAER LOS PARTIDOS: ", error);
+    }
+  };
+
+  await getWorldCupMatchesFromApi();
 };
