2108f6efb0cf7d6e2ffcf4933a11c5262b5c14ba
Enrique Arias <e.arias@rpalatam.com.pe>
2026-06-10 12:37:46 -0500

    feat js mundial ec

--- world-cup-2026-depor.js
+++ world-cup-2026-depor.js
@@ -96,7 +96,7 @@ const getWorldCupMatches = async (
 
   const getFlagPath = (slugTeam) =>
     `https://d3lyt7mv5ip2zx.cloudfront.net/polla2026/flags/${slugTeam.replace("rep-checa", "republica-checa").replace("bosnia-y-herzegovina", "bosnia-and-herzegovina").replace("rd-del-congo", "congo")}.png`;
-  // `https://cdna.elcomercio.pe/resources/dist/elcomercio/images/wc-2026-flags/${slugTeam}.png`;
+  // `https://cdna.elcomercio.pe/resources/dist/elcomercio/images/wc-2026-flags/${slugTeam}.png`; //
   // "https://cdna.elcomercio.pe/resources/dist/elcomercio/images/argentina.png";
 
   const getScoreValue = (goles, estado) =>
